abaf86bb78aacbc538ec0f02d323c8de6bd2ed57
Matheus Santana <130666046+mathsant-js@users.noreply.github.com>
2025-07-13 00:01:06 +0000

    Adicionando funcionalidade para concatenar dois dados fornecidos pelo usuário

--- resolucoes_code/concat_dados.py
+++ resolucoes_code/concat_dados.py
@@ -1,1 +1,7 @@
 # Vamos receber dois dados diferentes do usuário e concatena-los em uma única string?!
+
+info1 = input("Digite o primeiro dado: ")
+info2 = input("Digite o segundo dado: ")
+
+resultado = info1 + " " + info2
+print("Os dados concatenados são:", resultado)
